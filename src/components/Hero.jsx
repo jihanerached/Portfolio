c14af1fd58414cdeac0cb9993dc15c6cbe30161b
jihanerached <128765545+jihanerached@users.noreply.github.com>
2025-12-10 02:32:54 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915EFF]">Jihane</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            Final Year Engineering Student
+            Software Engineer
           </p>
           
           {/* Creative Welcome Text */}
